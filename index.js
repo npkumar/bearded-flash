5a7ee8e538f853e6853c1cd23a75b9fae8d0074c
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-04-18 21:42:03 +0800

    add favicon

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var app = express();
+
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
